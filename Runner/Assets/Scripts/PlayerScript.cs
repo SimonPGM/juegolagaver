b757baf6a89e9e1669887d9bc4eb6b9ffa7f6008
Daniel Estrada <daestradam@unal.edu.co>
2022-06-19 01:09:49 -0500

    Bloques

--- Runner/Assets/Scripts/PlayerScript.cs
+++ Runner/Assets/Scripts/PlayerScript.cs
@@ -28,15 +28,17 @@ public class PlayerScript : MonoBehaviour
         }
         
         _playerRigidBody.velocity = new Vector2(_moveSpeed, _playerRigidBody.velocity.y);
+
+        Debug.Log(touchingFloor);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         touchingFloor = true;
-        if (col.CompareTag("Obstacle"))
+        /*if (col.CompareTag("Obstacle"))
         {
             GameObject.Destroy(this.gameObject);
-        }
+        }*/
     }
 
     private void OnTriggerExit2D(Collider2D other)
--- Runner/Assets/Scripts/SceneController.cs
+++ Runner/Assets/Scripts/SceneController.cs
@@ -6,10 +6,15 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
+    public GameObject[] _BlockPreFab;
+    public float _playerPointer;
+    public float _safePlaceGenerator = 12;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _playerPointer = -7;
     }
 
     // Update is called once per frame
@@ -17,10 +22,22 @@ public class SceneController : MonoBehaviour
     {
         if (player != null)
         {
-           this.transform.position = new Vector3(player.transform.position.x,
-                this.transform.position.y,
-                this.transform.position.z);
+            this.transform.position = new Vector3(player.transform.position.x,
+                 this.transform.position.y,
+                 this.transform.position.z);
         }
-        
+        Debug.Log("While");/*
+        while (player != null && _playerPointer < player.transform.position.x + _safePlaceGenerator)
+        {
+            int indexBlock = Random.Range(0,_BlockPreFab.Length-1);
+            if (_playerPointer < 0)
+            {
+                indexBlock = 0;
+            }
+            //GameObject ObjectBlock = Instantiate(_BlockPreFab[indexBlock]);
+            //ObjectBlock.transform.SetParent(this.transform);
+            //Bloque block = ObjectBlock.GetComponent<Bloque>();
+            //ObjectBlock.transform.position = new Vector2(_playerPointer+block._size/2,0);
+        }*/
     }
 }
