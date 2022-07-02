00393940001ed4e8719d56fc7d8ec47ba689d4c1
Daniel Estrada <daestradam@unal.edu.co>
2022-07-01 21:05:12 -0500

    Sound powerup
    Tambien se agrego la eliminacion de bloques anteriores

--- Runner/Assets/Scripts/PlayerScript.cs
+++ Runner/Assets/Scripts/PlayerScript.cs
@@ -25,6 +25,15 @@ public class PlayerScript : MonoBehaviour
     private Renderer rend;
     private Color c;
 
+    [SerializeField]
+    private GameObject _chontaduro;
+    [SerializeField]
+    private GameObject _ehda;
+    [SerializeField]
+    private GameObject _eche;
+    [SerializeField]
+    private GameObject _death;
+
     void Start()
     {
         rend = GetComponent<Renderer>();
@@ -80,6 +89,8 @@ public class PlayerScript : MonoBehaviour
         if(other.gameObject.CompareTag("PowerUp1")){
             Destroy(other.gameObject);
             if (!_isInvincible){
+                Instantiate(_eche);
+                Instantiate(_chontaduro);
                 _isInvincible = true;
                 StartCoroutine("Invulnerable");
             }
